06ac1bf7d83cc6dcdc489599326bd150b5cbebc5
Elliot Marret <elliotmrt@icloud.com>
2024-01-25 16:48:27 +0100

    Watch List back end done and some scss

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+50.times do
+  Movie.create!(
+    title: Faker::Movie.title,
+    overview: Faker::Lorem.sentence(word_count: 10),
+    poster_url: Faker::Internet.url,
+    rating: rand(5.0..10.0).round(1)
+  )
+end
+
+Movie.create!(
+  title: "Wonder Woman 1984",
+  overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s",
+  poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg",
+  rating: 6.9
+)
